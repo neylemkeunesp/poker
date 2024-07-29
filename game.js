bd99ba4b1cb02eeab7384f8b8b90c4809cc2198c
Ney Lemke <ney.nlemke@gmail.com>
2024-07-28 22:10:49 -0300

    Added the dealCards function to the import statement in game.js.

--- game.js
+++ game.js
@@ -28,7 +28,7 @@ import { createDeck, shuffle, updateUI, formatCard, startInitialBettingRound, ch
         let currentBet = 0;
         let currentBettingRound = 0;
 
-import { createDeck, shuffle, updateUI, formatCard, startInitialBettingRound, checkSinglePlayerLeft, endGame, nextPlayer, checkRoundEnd, placeBet, evaluateHand, computerAction, showdown } from './shared.js';
+import { createDeck, shuffle, updateUI, formatCard, startInitialBettingRound, checkSinglePlayerLeft, endGame, nextPlayer, checkRoundEnd, placeBet, evaluateHand, computerAction, showdown, dealCards } from './shared.js';
 
         function startBettingRound() {
             for (let i = 0; i < players.length; i++) {
