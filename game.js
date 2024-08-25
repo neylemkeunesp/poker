4e7b0d497e2eac129705dc9024b721386ad8012b
neylemkeunesp <ney.lemke@unesp.br>
2024-08-25 09:50:31 -0300

    refactor: remove unused deck variable from game.js

--- game.js
+++ game.js
@@ -2,7 +2,6 @@ import { createDeck, shuffle, updateUI, formatCard, checkSinglePlayerLeft, endGa
 
 const suits = ['♠', '♥', '♦', '♣'];
 const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-let deck = [];
 let communityCards = [];
 let currentPlayerIndex = 0;
 let pot = 0;
