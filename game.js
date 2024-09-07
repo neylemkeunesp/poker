2097c747705b33ffaeb3b0704677ed34dd548014
neylemkeunesp <ney.lemke@unesp.br>
2024-09-07 17:55:30 -0300

    fix: initialize deck array before creating a new deck in startNewGame function

--- game.js
+++ game.js
@@ -21,6 +21,7 @@ function startNewGame() {
         console.error("No players available to start the game.");
         return;
     }
+    deck=[]
     deck = createDeck();
     if (deck.length > 0) {
         shuffle(deck);
